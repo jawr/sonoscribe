e39d8d1507b5529afe4bcda3eb3bc0c8175c348c
Jess Lawrence <jess@salesbricks.com>
2023-07-13 16:06:53 +0900

    add readme and some cleanup

---
+++ README.md
@@ -0,0 +1,67 @@
+## Sonoscribe
+
+Turn an ebook into a summarized audiobook.
+
+Stiches together some awesome technologies:
+
+- [pszemraj/document-summarization](https://huggingface.co/spaces/pszemraj/document-summarization) Text summarization
+- [rhasspy/piper](https://github.com/rhasspy/piper) Text To Speech
+
+### Setup
+
+First install dependencies:
+
+```
+brew install ffmpeg
+```
+
+Next setup the python environment:
+
+```
+pipenv
+pipenv install
+```
+
+Next setup piper. If you can compile/use already compiled do so, on Apple Silicon I had to use a Docker solution:
+
+```
+git clone https://github.com/rhasspy/piper.git
+cd piper
+docker buildx build --target build -t piper:latest .
+```
+
+Run the container in another tab so it runs indefinitely:
+
+```
+docker run -it --entrypoint bash piper:latest
+```
+
+Download the voices you want to use (currently hardcoded to `en_GB-alba-medium`):
+
+```
+mkdir piper-voices
+curl -OL https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_GB/alba/medium/en_GB-alba-medium.onnx
+curl -OL https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_GB/alba/medium/en_GB-alba-medium.onnx.json
+```
+
+### Running
+
+Add an epub into epubs with the following format:
+
+```
+author-title.epub
+william_shakespeare-king_lear.epub
+```
+
+And finally run:
+
+```
+pipenv run python main.py ebooks/william_shakespeare-king_lear.epub
+```
+
+Summarized text and tagged mp3 will be output to:
+
+```
+summaries/william_shakespeare-king_lear.txt
+summaries/william_shakespeare-king_lear.mp3
+```
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import os
+import subprocess
 import sys
-from ebooklib import ITEM_DOCUMENT, epub
 from bs4 import BeautifulSoup
+from ebooklib import ITEM_DOCUMENT, epub
 from textsum.summarize import Summarizer
+from pydub import AudioSegment
+
+# name of the model to use for summarization
+SUMMARY_MODEL_NAME = "pszemraj/long-t5-tglobal-base-16384-booksci-summary-v1"
+
+# token batch length to use for summarization
+SUMMARY_TOKEN_BATCH_LENGTH = 3072
 
 
 def extract_text_from_ebook(ebook_path: str) -> str:
@@ -26,28 +34,97 @@ def extract_text_from_ebook(ebook_path: str) -> str:
     return book_text
 
 
-def summarize_text(
-    text: str,
-    model_name: str = "pszemraj/led-base-book-summary",
-    token_batch_length: int = 4096,
-) -> str:
+def summarize_text(text: str, output_file: str) -> str:
+    if os.path.exists(output_file):
+        with open(output_file, "r", encoding="utf-8") as file:
+            return file.read()
+
     summarizer = Summarizer(
-        model_name_or_path=model_name,
-        token_batch_length=token_batch_length,
+        model_name_or_path=SUMMARY_MODEL_NAME,
+        token_batch_length=SUMMARY_TOKEN_BATCH_LENGTH,
     )
     summary = summarizer.summarize_string(text)
-    print(f"Summarized text to {len(summary)} characters")
+
+    with open(output_file, "w", encoding="utf-8") as file:
+        file.write(summary)
+
+    print(f"Summarized text saved to: {output_file}")
     return summary
 
 
-def save_summarized_text(filename: str, summarized_text: str) -> None:
-    base_dir = "summaries"
-    os.makedirs(base_dir, exist_ok=True)
-    output_file = os.path.join(base_dir, f"{os.path.splitext(filename)[0]}.txt")
+def get_docker_container_id(image_name: str) -> str:
+    command = "docker ps --filter ancestor=" + image_name + " --format '{{.ID}}'"
+    output = subprocess.check_output(command, shell=True, universal_newlines=True)
+    container_id = output.strip()
+    return container_id
 
-    with open(output_file, "w", encoding="utf-8") as file:
-        file.write(summarized_text)
-    print(f"Summarized text saved to: {output_file}")
+
+def docker_copy(source_path: str, destination_path: str) -> None:
+    subprocess.run(["docker", "cp", source_path, destination_path])
+
+
+def docker_exec(container_id: str, command: str) -> None:
+    subprocess.run(["docker", "exec", "-it", container_id, "/bin/bash", "-c", command])
+
+
+def generate_speech(filename: str, container_id: str):
+    # Parameters
+    destination_filename = f"{filename}.txt"
+    output_filename = f"{filename}.wav"
+
+    if os.path.exists(f"./summaries/{output_filename}"):
+        return
+
+    piper_command = f"cd /dist/piper && cat {destination_filename} | ./piper --model en_GB-alba-medium.onnx --output_file {output_filename}"
+
+    # Command 1: docker cp
+    docker_copy(
+        f"./summaries/{destination_filename}",
+        f"{container_id}:/dist/piper/{destination_filename}",
+    )
+
+    # Command 2: docker exec
+    docker_exec(container_id, piper_command)
+
+    # Command 3: docker cp
+    docker_copy(
+        f"{container_id}:/dist/piper/{output_filename}",
+        f"./summaries/{output_filename}",
+    )
+
+
+def get_filename_from_path(path: str) -> str:
+    # Extract the base filename from the path
+    filename = os.path.basename(path)
+    # Remove the file extension
+    filename_without_extension = os.path.splitext(filename)[0]
+    return filename_without_extension
+
+
+def convert_wav_to_mp3(filename: str) -> None:
+    tags = {
+        "title": filename.split("-")[1].replace("_", " ").strip().title(),
+        "artist": filename.split("-")[0].replace("_", " ").strip().title(),
+        "album": "Book Summary",
+    }
+
+    wav_file = f"./summaries/{filename}.wav"
+    mp3_file = f"./summaries/{filename}.mp3"
+
+    audio = AudioSegment.from_wav(wav_file)
+
+    audio.export(mp3_file, format="mp3", tags=tags)
+
+
+def setup_piper(container_id: str) -> None:
+    docker_copy(
+        "./piper-voices/en_GB-alba-medium.onnx",
+        f"{container_id}:/dist/piper/en_GB-alba-medium.onnx",
+    )
+    docker_copy(
+        "./piper-voices/en_GB-alba-medium.onnx.json",
+        f"{container_id}:/dist/piper/en_GB-alba-medium.onnx.json",
+    )
 
 
 def main():
@@ -58,10 +135,20 @@ def main():
     ebook_path = sys.argv[1]
     text: str = extract_text_from_ebook(ebook_path)
 
-    summarized_text = summarize_text(text)
+    filename = get_filename_from_path(ebook_path)
 
-    filename = os.path.basename(ebook_path)
-    save_summarized_text(filename, summarized_text)
+    base_dir = "summaries"
+    os.makedirs(base_dir, exist_ok=True)
+    summary_file = os.path.join(base_dir, f"{filename}.txt")
+
+    summarize_text(text, summary_file)
+
+    container_id = get_docker_container_id("piper")
+    setup_piper(container_id)
+
+    generate_speech(filename, container_id)
+
+    convert_wav_to_mp3(filename)
 
 
 if __name__ == "__main__":
